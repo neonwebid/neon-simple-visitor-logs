92abecd4e2f39be82d7587e80801ca29fa20c4b1
Wakhid Wichaksono <wichaksono@users.noreply.github.com>
2026-02-02 17:10:28 +0700

    Add IP Manager to VisitorLogAdminMenu

--- src/VisitorLogAdminMenu/VisitorLogAdminMenu.php
+++ src/VisitorLogAdminMenu/VisitorLogAdminMenu.php
@@ -3,18 +3,20 @@
 namespace NeonWebId\SimpleVisitorLogs\VisitorLogAdminMenu;
 
 use NeonWebId\SimpleVisitorLogs\VisitorLogAdminMenu\Pages\ASNManager;
-use NeonWebId\SimpleVisitorLogs\VisitorLogAdminMenu\Pages\ExcludeUserAgents;
+use NeonWebId\SimpleVisitorLogs\VisitorLogAdminMenu\Pages\IPManager;
 use NeonWebId\SimpleVisitorLogs\VisitorLogAdminMenu\Pages\VisitorLogs;
 
 final class VisitorLogAdminMenu
 {
     private VisitorLogs $visitorLogs;
     private ASNManager $asnManager;
+    private IPManager $IPManager;
 
     public function __construct()
     {
         $this->visitorLogs       = new VisitorLogs();
         $this->asnManager        = new ASNManager();
+        $this->IPManager = new IPManager();
         add_action('admin_menu', [$this, 'register']);
     }
 
@@ -54,6 +56,16 @@ final class VisitorLogAdminMenu
             'manage_options',
             'svl-asn-manager',
             [$this->asnManager, 'render']
+        );
+
+        // Submenu: IP Manager
+        add_submenu_page(
+            'svl-visitor-logs',
+            'IP Manager',
+            'IP Manager',
+            'manage_options',
+            'svl-ip-manager',
+            [$this->IPManager, 'render']
         );
 
     }
